7f03ec265b6a20de2ec70aadbfa1768957623d86
Sultan <ss.softwareit@gmail.com>
2022-04-17 22:50:51 +0600

    added_regex

--- script.js
+++ script.js
@@ -2,6 +2,32 @@ const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
+
+	
+function ValidateEmailAddress(emailString) 
+{
+	var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//	var emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+		return !!emailString && typeof emailString === 'string'
+			&& emailString.match(emailRegex);
+}
+
+function CheckEmail(emailString)
+{
+	//get result as true/false
+	var Result= ValidateEmailAddress(emailString);
+	
+	if(Result)
+	{
+		document.getElementById("output").innerHTML="Valid Email Id";
+	}
+	else
+	{
+		document.getElementById("output").innerHTML="NOT a Valid Email Id";
+	}
+}
+
+
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
 });
